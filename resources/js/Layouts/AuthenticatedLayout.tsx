85b02f5651c30f4cd4601c7a34afc82ae15c60d3
Sebastian Arrieta Villarreal <25210925+sarrietav-dev@users.noreply.github.com>
2025-02-24 13:22:36 -0500

    feat: enhance logo display in AuthenticatedLayout with transition effects for sidebar collapse

--- resources/js/Layouts/AuthenticatedLayout.tsx
+++ resources/js/Layouts/AuthenticatedLayout.tsx
@@ -10,18 +10,20 @@ export default function Authenticated({
         <div className="min-h-screen bg-gray-100 lg:flex lg:items-stretch">
             <nav
                 className={cn(
-                    'fixed bottom-0 left-0 w-full rounded-t-lg bg-gray-900 px-10 pt-2 transition-all duration-300 lg:relative lg:top-0 lg:bottom-auto lg:w-75 lg:rounded-t-none lg:rounded-r-lg lg:px-6 lg:py-4 lg:pl-0',
+                    'fixed bottom-0 left-0 w-full rounded-t-lg bg-gray-900 px-10 pt-2 transition-all duration-300 lg:relative lg:top-0 lg:bottom-auto lg:w-75 lg:rounded-t-none lg:rounded-r-lg lg:px-6 lg:py-10 lg:pl-0',
                     collapsed && 'collapsed group lg:w-27 lg:px-4 lg:pl-0',
                 )}
             >
                 <ul className="flex justify-between text-gray-300 lg:h-full lg:flex-col">
-                    <div className="mb-16 hidden cursor-pointer flex-row items-center justify-start gap-2 border-l-4 border-l-transparent px-8 py-4 lg:flex">
+                    <div className="relative mb-16 hidden w-40 cursor-pointer flex-row items-center justify-start gap-2 border-l-4 border-l-transparent py-4 transition-all group-[.collapsed]:w-24 lg:flex">
                         <img
-                            src={
-                                collapsed
-                                    ? '/images/logo-small.svg'
-                                    : '/images/logo-large.svg'
-                            }
+                            className="absolute inset-0 left-8 h-full w-full opacity-100 transition-all duration-300 group-[.collapsed]:translate-y-full group-[.collapsed]:opacity-0"
+                            src="/images/logo-large.svg"
+                            alt=""
+                        />
+                        <img
+                            className="absolute inset-0 h-full w-full opacity-0 transition-all duration-300 group-[.collapsed]:translate-y-0 group-[.collapsed]:opacity-100"
+                            src="/images/logo-small.svg"
                             alt=""
                         />
                     </div>
