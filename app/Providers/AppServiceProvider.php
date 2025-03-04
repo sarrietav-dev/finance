672768a1ba45d0a6bb67cbef592e97daaf3cefa8
Sebastian Arrieta Villarreal <25210925+sarrietav-dev@users.noreply.github.com>
2025-03-04 13:53:28 -0500

    feat: integrate Laravel Telescope for enhanced debugging and monitoring

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Telescope\TelescopeServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -12,7 +13,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        if ($this->app->environment('local') && class_exists(\Laravel\Telescope\TelescopeServiceProvider::class)) {
+
+            $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
+
+            $this->app->register(TelescopeServiceProvider::class);
+        }
     }
 
     /**
