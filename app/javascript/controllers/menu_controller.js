84fda77f4b1d11f920ceb4c6ddad4e6970488de6
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com.>
2025-04-12 16:18:14 -0500

    Fixed sidebar menu

--- app/javascript/controllers/menu_controller.js
+++ app/javascript/controllers/menu_controller.js
@@ -5,8 +5,22 @@ export default class extends Controller {
   static classes = ["collapsed", "collapsedMain"];
   static values = { collapsed: Boolean };
 
+  connect() {
+    const stored = localStorage.getItem("menuCollapsed");
+
+    // Normalize to boolean
+    this.collapsedValue = stored === "true";
+
+    this.applyMenuState();
+  }
+
   toggle() {
-    console.log(this.collapsedClasses.join(" "));
+    this.collapsedValue = !this.collapsedValue;
+    localStorage.setItem("menuCollapsed", this.collapsedValue);
+    this.applyMenuState();
+  }
+
+  applyMenuState() {
     if (this.collapsedValue) {
       this.sidebarTarget.classList.add(...this.collapsedClasses);
       this.mainTarget.classList.add(...this.collapsedMainClasses);
@@ -14,7 +28,5 @@ export default class extends Controller {
       this.sidebarTarget.classList.remove(...this.collapsedClasses);
       this.mainTarget.classList.remove(...this.collapsedMainClasses);
     }
-
-    this.collapsedValue = !this.collapsedValue;
   }
 }
