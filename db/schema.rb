e86aaad4c7855cdf6889f6cf8b04d09dc7748647
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com.>
2025-04-12 16:30:49 -0500

    add auth

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_12_033923) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_12_213017) do
+  create_table "sessions", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "ip_address"
+    t.string "user_agent"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
   create_table "transactions", force: :cascade do |t|
     t.string "avatar"
     t.string "name"
@@ -21,4 +30,14 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_12_033923) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email_address", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email_address"], name: "index_users_on_email_address", unique: true
+  end
+
+  add_foreign_key "sessions", "users"
 end
