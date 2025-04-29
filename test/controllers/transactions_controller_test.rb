954667b3067e4608231de18b77ffc67ac106f8a5
Sebastian Arrieta Villarreal <25210925+sarrietav-dev@users.noreply.github.com>
2025-04-28 21:55:48 -0500

    style: format files

--- test/controllers/transactions_controller_test.rb
+++ test/controllers/transactions_controller_test.rb
@@ -17,7 +17,7 @@ class TransactionsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create transaction" do
     assert_difference("Transaction.count") do
-      post transactions_url, params: { transaction: { amount: @transaction.amount, avatar: @transaction.avatar, category: @transaction.category, date: @transaction.date, name: @transaction.name, recurring: @transaction.recurring } }
+      post transactions_url, params: {transaction: {amount: @transaction.amount, avatar: @transaction.avatar, category: @transaction.category, date: @transaction.date, name: @transaction.name, recurring: @transaction.recurring}}
     end
 
     assert_redirected_to transaction_url(Transaction.last)
@@ -34,7 +34,7 @@ class TransactionsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update transaction" do
-    patch transaction_url(@transaction), params: { transaction: { amount: @transaction.amount, avatar: @transaction.avatar, category: @transaction.category, date: @transaction.date, name: @transaction.name, recurring: @transaction.recurring } }
+    patch transaction_url(@transaction), params: {transaction: {amount: @transaction.amount, avatar: @transaction.avatar, category: @transaction.category, date: @transaction.date, name: @transaction.name, recurring: @transaction.recurring}}
     assert_redirected_to transaction_url(@transaction)
   end
 
