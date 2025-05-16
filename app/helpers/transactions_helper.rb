ce803d4b95d12fa9188f2e1862e57906f52d90e7
Sebastian Arrieta Villarreal <25210925+sarrietav-dev@users.noreply.github.com>
2025-05-16 08:40:45 -0500

    Move amount parsing into helper

--- app/helpers/transactions_helper.rb
+++ app/helpers/transactions_helper.rb
@@ -1,3 +1,11 @@
 module TransactionsHelper
   include Pagy::Frontend
+
+  def parse_amount(amount)
+    if amount < 0
+      "-$#{"%.2f" % amount.abs}"
+    else
+      "$#{"%.2f" % amount}"
+    end
+  end
 end
