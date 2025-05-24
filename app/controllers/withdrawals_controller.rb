c9658c408a72e37f7e06ffb238960770b68183c9
Sebastian Arrieta Villarreal <25210925+sarrietav-dev@users.noreply.github.com>
2025-05-23 20:43:22 -0500

    Refactor withdrawals to use Turbo Streams and partial form

--- app/controllers/withdrawals_controller.rb
+++ app/controllers/withdrawals_controller.rb
@@ -4,5 +4,25 @@ class WithdrawalsController < ApplicationController
   end
 
   def create
+    @pot = Pot.find(params[:pot_id])
+    @pot.total -= withdrawal_params[:amount].to_f
+
+    respond_to do |format|
+      if @pot.save
+        format.turbo_stream
+      else
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.update("new_withdrawal",
+            partial: "withdrawals/form"),
+            status: :unprocessable_entity
+        end
+      end
+    end
+  end
+
+private
+
+  def withdrawal_params
+    params.permit(:amount, :pot_id)
   end
 end
