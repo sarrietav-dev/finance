1a0b065a410d748464e97468ca23d06b58732193
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2025-05-31 13:16:38 -0500

    Upgrade to Rails 8 and Active Storage, update dependencies
    - Switch bin/dev to use Rails server directly
    - Remove custom font asset path from application config
    - Add Rails 8 framework defaults initializer
    - Add Active Storage migrations
    - Update Gemfile.lock with new gem versions
    - Update environment configs for style and Rails 8 changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_03_215759) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_31_181347) do
   create_table "pots", force: :cascade do |t|
     t.string "name"
     t.decimal "target"
