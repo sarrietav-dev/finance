3e6761228e690a746e694d732668b89f9b1cddf5
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2026-04-01 10:38:50 -0500

    chore: improve kamal config

--- db/schema.rb
+++ db/schema.rb
@@ -10,22 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_01_213557) do
+ActiveRecord::Schema[8.1].define(version: 2025_09_01_213557) do
   create_table "budgets", force: :cascade do |t|
     t.string "category"
+    t.integer "category_id"
+    t.datetime "created_at", null: false
     t.decimal "maximum"
     t.string "theme"
-    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "category_id"
     t.integer "user_id", null: false
     t.index ["category_id"], name: "index_budgets_on_category_id"
     t.index ["user_id"], name: "index_budgets_on_user_id"
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string "name"
     t.datetime "created_at", null: false
+    t.string "name"
     t.datetime "updated_at", null: false
     t.integer "user_id", null: false
     t.index ["name"], name: "index_categories_on_name", unique: true
@@ -33,44 +33,44 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_01_213557) do
   end
 
   create_table "pots", force: :cascade do |t|
+    t.datetime "created_at", null: false
     t.string "name"
     t.decimal "target"
-    t.decimal "total", default: "0.0"
     t.string "theme"
-    t.datetime "created_at", null: false
+    t.decimal "total", default: "0.0"
     t.datetime "updated_at", null: false
     t.integer "user_id", null: false
     t.index ["user_id"], name: "index_pots_on_user_id"
   end
 
   create_table "sessions", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.string "ip_address"
-    t.string "user_agent"
     t.datetime "created_at", null: false
+    t.string "ip_address"
     t.datetime "updated_at", null: false
+    t.string "user_agent"
+    t.integer "user_id", null: false
     t.index ["user_id"], name: "index_sessions_on_user_id"
   end
 
   create_table "transactions", force: :cascade do |t|
-    t.string "avatar"
-    t.string "name"
-    t.string "category"
-    t.datetime "date"
     t.decimal "amount"
-    t.boolean "recurring", default: false, null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string "avatar"
+    t.string "category"
     t.integer "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "date"
+    t.string "name"
+    t.boolean "recurring", default: false, null: false
+    t.datetime "updated_at", null: false
     t.integer "user_id", null: false
     t.index ["category_id"], name: "index_transactions_on_category_id"
     t.index ["user_id"], name: "index_transactions_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
     t.string "email_address", null: false
     t.string "password_digest", null: false
-    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
